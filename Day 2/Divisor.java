da826905ea175527d4231970b2cc5395a18cc046
M.Ganesha Moorthi <152759878+Ganeshmoorthii@users.noreply.github.com>
2024-04-25 06:42:53 +0530

    Create Divisor.java

---
+++ Day 2/Divisor.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static List<Integer> printDivisors(int n) {
+        List<Integer> divisors = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+            }
+        }
+        
+        return divisors;
+    }
+}
