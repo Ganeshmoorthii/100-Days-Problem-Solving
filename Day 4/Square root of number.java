734b24fe3e35a5117706baf1b46a7b72f2608a84
M.Ganesha Moorthi <152759878+Ganeshmoorthii@users.noreply.github.com>
2024-04-26 21:35:51 +0530

    Create Square root of number.java

---
+++ Day 4/Square root of number.java
@@ -0,0 +1,12 @@
+/*You are required to complete
+this function*/
+
+// Function to find square root
+// x: element to find square root
+class Solution
+{
+     long floorSqrt(long x)
+	 {
+		return (long)(Math.sqrt(x));
+	 }
+}
